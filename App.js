35f7c31975a1f9288c1c16f9e3a0844c94b027e1
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-07-23 10:24:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./task3/Login";
+import Register from "./task3/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Register">Register</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/Register" component={Register} />
+
+          <Route path="/Login" component={Login} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
